917c1d2566ea6851ed3b86f3d176d7a21d8f5329
Eivind Norling <Eivind.Norling@tidsbanken.no>
2022-07-26 22:36:55 +0200

    Refactor: Started refactoring

--- EwyBot/Program.cs
+++ EwyBot/Program.cs
@@ -1,6 +1,6 @@
-﻿using System.Text;
-using Discord;
+﻿using Discord;
 using Discord.WebSocket;
+using EwyBot.Commands;
 
 namespace EwyBot;
 
@@ -9,11 +9,18 @@ public static class Program
     public static Task Main(string[] args) => MainAsync();
 
     private static DiscordSocketClient _client;
+    
+    public static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
 
     private static async Task MainAsync()
     {
         var client = new DiscordSocketClient();
-        client.MessageReceived += HandleCommandAsync;
+        
+        client.MessageReceived += CommandManager.HandleCommandAsync;
         
         client.Log += Log;
         
@@ -23,61 +30,5 @@ public static class Program
         await client.StartAsync();
 
         await Task.Delay(-1);
-    }
-    
-    private static Task HandleCommandAsync(SocketMessage message)
-    {
-        if (message is not SocketUserMessage)
-        {
-            return Task.CompletedTask;
-        }
-        
-        if (message.Author.IsBot)
-        {
-            return Task.CompletedTask;
-        }
-
-        if (!message.Content.StartsWith('!'))
-        {
-            return Task.CompletedTask;
-        }
-
-        var chatInput = message.Content.Substring(1, message.Content.Length - 1).ToLower();
-        
-        switch (chatInput)
-        {
-            case "test":
-                SendCommandsResponse(message, $"Hello {message.Author.Mention}! This is a test message.");
-                break;
-            case "help":
-                SendCommandsResponse(message, 
-                    "Here is a list of all commands: " 
-                    + "\n !test - test command" + " " 
-                    + "\n !help - help command" + " "
-                );
-                break;
-            default:
-                SendUnknownCommandResponse(message);
-                break;
-        }
-
-        return Task.CompletedTask;
-    }
-
-    private static void SendCommandsResponse(SocketMessage message, string response)
-    {
-        message.Channel.SendMessageAsync(response);
-        Log(new LogMessage(LogSeverity.Info, "COMMAND", response));
-    }
-
-    private static void SendUnknownCommandResponse(SocketMessage message)
-    {
-        SendCommandsResponse(message, $"**{message.Content}** is an invalid command. Please use !help for a list of available commands.");
-    }
-
-    private static Task Log(LogMessage msg)
-    {
-        Console.WriteLine(msg.ToString());
-        return Task.CompletedTask;
     }
 }
