5349762e83916b3b32e12213a5b57b6d3ae46517
Ewy <EwyBoy@gmail.com>
2023-04-23 20:28:21 +0200

    EwyBot v2.0

---
+++ EwyBot/DiscordBotService.cs
@@ -0,0 +1,55 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace EwyBot;
+
+public class DiscordBotService : BackgroundService
+{
+    private readonly DiscordSocketClient _client;
+
+    public DiscordBotService()
+    {
+        _client = new DiscordSocketClient(
+            new DiscordSocketConfig {
+                GatewayIntents = GatewayIntents.MessageContent | GatewayIntents.Guilds | GatewayIntents.GuildMessages,
+            }
+        );
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var secret = Environment.GetEnvironmentVariable("secret");
+
+        _client.Log += LogAsync;
+        _client.MessageReceived += HandleMessageReceivedAsync;
+        _client.Ready += Client_ReadyAsync;
+
+        await _client.LoginAsync(TokenType.Bot, secret);
+        await _client.StartAsync();
+
+        await Task.Delay(Timeout.Infinite, stoppingToken);
+    }
+
+    private static Task LogAsync(LogMessage arg)
+    {
+        Console.WriteLine(arg);
+        return Task.CompletedTask;
+    }
+
+    private async Task Client_ReadyAsync()
+    {
+        await _client.SetGameAsync("Listening to !hello");
+        await _client.SetStatusAsync(UserStatus.Online);
+    }
+
+    private static async Task HandleMessageReceivedAsync(SocketMessage message)
+    {
+        if (message.Author.IsBot)
+            return;
+        
+        if (message.Content.StartsWith("!hello"))
+        {
+            await message.Channel.SendMessageAsync("Hello there!");
+        }
+    }
+}
--- EwyBot/Program.cs
+++ EwyBot/Program.cs
@@ -1,61 +1,8 @@
-﻿using Discord;
-using Discord.Commands;
-using Discord.WebSocket;
-using EwyBot.Commands;
-using Microsoft.Extensions.DependencyInjection;
+using EwyBot;
 
-namespace EwyBot;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHostedService<DiscordBotService>();
 
-public class Program
-{
-    
-    public static Task Main()
-    {
-        return new Program().MainAsync();
-    }
-
-    public async Task MainAsync()
-    {
-        using var services = ConfigureServices();
-
-        var client = services.GetRequiredService<DiscordSocketClient>();
-        
-        client.Log += LogManager.Log;
-
-        services.GetRequiredService<CommandService>().Log += Log;
-        
-        var secret = Environment.GetEnvironmentVariable("secret");
-        
-        await client.LoginAsync(TokenType.Bot, secret);
-        await client.StartAsync();
-        
-        await services.GetRequiredService<CommandCenter>().InitializeAsync();
-
-        await Task.Delay(-1);
-    }
-
-    public ServiceProvider ConfigureServices()
-    {
-        return new ServiceCollection()
-            .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig 
-            { 
-                MessageCacheSize = 500,
-                LogLevel = LogSeverity.Info
-            }))
-            .AddSingleton(new CommandService(new CommandServiceConfig
-            { 
-                LogLevel = LogSeverity.Info,
-                DefaultRunMode = RunMode.Async,
-                CaseSensitiveCommands = false 
-            }))
-            .AddSingleton<CommandCenter>()
-            .BuildServiceProvider();
-    }
-
-    private Task Log(LogMessage log)
-    {
-        Console.WriteLine(log.ToString());
-        return Task.CompletedTask;
-    }
-    
-}
+var app = builder.Build();
+app.MapGet("/", () => "Hello World!");
+await app.RunAsync();
